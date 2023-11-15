c425b7c5939eafa51d3f9d4046b367063f49714e
Garth Bjerk <garth@bakkenbaeck.no>
2023-11-15 17:10:34 +0100

    Build out the func repo

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 Highlight.js language definition for TON FunC
+
+# FunC - a langauge grammar for [highlight.js](https://highlightjs.org/)
+
+## Usage
+
+Simply include the Highlight.js library in your webpage or Node app, then load this module.
+
+## License
+
+Highlight.js is released under the MIT License. See [LICENSE](./License) file
+for details.
+
+## Links
+
+- The official site for the Highlight.js library is <https://highlightjs.org/>.
+- The Highlight.js GitHub project: <https://github.com/highlightjs/highlight.js>
+- Learn more about FunC: <https://docs.ton.org/develop/func/overview>
--- src/func.js
+++ src/func.js
@@ -1,28 +1,31 @@
 /*
 Language: FunC
 Author: Gunnar Grimnes <gunnar@bakkenbaeck.no>
+Contributors: Garth Bjerk <garth@bakkenbaeck.no>,
 Description: A high-level language FunC is used to program smart contracts on TON.
 Website: https://docs.ton.org/develop/func/overview
 */
-export default function(hljs) {
+export default function (hljs) {
   return {
     name: "FunC",
     keywords: {
-	  type: "int slice cell builder tuple cont",
-	  keyword: "null asm forall impure inline inline_ref method_id var if return throw_unless do until while ifnot",
-	  built_in: "main recv_internal recv_external run_ticktock",
-	},
+      type: "int slice cell builder tuple cont",
+      keyword:
+        "null asm forall impure inline inline_ref method_id var if return throw_unless do until while ifnot",
+      built_in: "main recv_internal recv_external run_ticktock",
+    },
     contains: [
-	  hljs.C_NUMBER_MODE,
-	  {
-		scope: 'string',
-		begin: '"', end: '"'
+      hljs.C_NUMBER_MODE,
+      {
+        scope: "string",
+        begin: '"',
+        end: '"',
       },
-	  hljs.COMMENT(
-      '{-', // begin
-      '-}', // end
+      hljs.COMMENT(
+        "{-", // begin
+        "-}" // end
       ),
-	  hljs.COMMENT(';;', /[\n]/)
-	]
-  }
+      hljs.COMMENT(";;", /[\n]/),
+    ],
+  };
 }
